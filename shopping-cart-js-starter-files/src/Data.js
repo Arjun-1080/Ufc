32ff97ec392fde76efbf8957ef04226979b945c2
Arjun-1080 <mallikarjunsawakar06@gmail.com>
2024-03-03 12:57:37 +0530

    jnl

---
+++ shopping-cart-js-starter-files/src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Gloves",
+    price: 1600,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/gloves.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Jacket",
+    price: 3000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/jacket.jpeg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 1500,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/shirt.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Wrestling helmet",
+    price: 7000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/helmet.jpg",
+  },
+];
---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Gloves",
+    price: 1600,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "src/gloves.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Jacket",
+    price: 3000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "src/jacket.jpeg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 1500,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "src/shirt.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Wrestling helmet",
+    price: 7000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "src/helmet.jpg",
+  },
+];
